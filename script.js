e7a31773a211e9aede1540c96ab7f94baefd2473
sharvesh019 <ss9034@srmist.edu.in>
2025-05-04 23:52:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Typewriter effect for title
+const text = "Biomedical Engineer";
+const quote = '"Code is creativity made <span>visible</span>."';
+let i = 0;
+const speed = 100;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typed-text").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+window.onload = () => {
+  typeWriter();
+  document.getElementById("quote-text").innerHTML = quote;
+};
 
